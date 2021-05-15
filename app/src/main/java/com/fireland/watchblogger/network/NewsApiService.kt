d776d751ac1068c9b707ac2a997f085ad6e2d477
Eduardo Chavez <mccartney3150@gmail.com>
2021-05-14 19:26:54 -0600

    Added git actions

--- app/src/main/java/com/fireland/watchblogger/network/NewsApiService.kt
+++ app/src/main/java/com/fireland/watchblogger/network/NewsApiService.kt
@@ -11,5 +11,5 @@ class NewsApiServiceImpl @Inject constructor() : NewsApiService {
     private val endpointService = EndpointFactory.makeHttpService()
 
     override suspend fun getLatestWatchNews() =
-        endpointService.getWatchArticles(date = "2020-12-17")
+        endpointService.getWatchArticles(date = "2021-05-13")
 }
